676bf88c8b1eaa376bed1b8b0513006d3d647a14
Winston Tokuhisa <tokuhisawinston@outlook.com>
2018-07-03 16:02:05 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,10 +1,3 @@
-# def oxford_comma(array)
-#   if array.length == 1
-#     return array[0]
-#   elsif array.length == 2
-#     return array[0] + " and " + array[1]
-#   end
-# end
 def oxford_comma(array)
   oxford = ''
 
